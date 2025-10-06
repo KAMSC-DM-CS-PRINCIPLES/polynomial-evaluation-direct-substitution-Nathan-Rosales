53e755038200d8d79ab0c3258ba8960986d76c76
Nathan-Rosales <nathan08.rosales@gmail.com>
2025-10-05 23:38:52 -0400

    a
    a

--- polynomial.py
+++ polynomial.py
@@ -2,11 +2,33 @@ def evaluate_polynomial(degree, x, constant_term, *coefficients):
     # TODO: Implement polynomial evaluation using direct substitution method
     # TODO: Print step-by-step evaluation (S0, S1, S2, etc.)
     # TODO: Return final polynomial result
-    pass
+
+    S = constant_term
+    k = 1
+
+    a = 0
+    while k<=degree:
+        num = coefficients[0][a]
+        S += num*(x**k)
+        a += 1
+        k += 1
+    
+    print(S)
+    
 
 if __name__ == "__main__":
     # TODO: Add main program loop
     # TODO: Get user input for degree, x, constant term, and coefficients
     # TODO: Call evaluate_polynomial function
     # TODO: Ask user if they want to run again
-    pass
+
+    degree = int(input("Degree of the polynomial: "))
+    x = int(input("Value of x: "))
+    constant_term = int(input("Value of constant term: "))
+
+    coefficients = ()
+
+    for i in range(1, degree+1):
+        coefficients += (int(input(f"Coefficient of the x^{i} term:")),)
+    
+    evaluate_polynomial(degree, x, constant_term, coefficients)
