98e971f5308e91c264cebe115558c018f746dda0
Nathan-Rosales <nathan08.rosales@gmail.com>
2025-10-06 11:49:03 -0400

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -5,7 +5,7 @@ def evaluate_polynomial(degree, x, constant_term, *coefficients):
     # TODO: Return final polynomial result
 
     if(len(coefficients)!=degree):
-        raise ValueError
+        raise ValueError(f"need "[degree]"coefficent(s)")
 
     S = constant_term
     k = 1
