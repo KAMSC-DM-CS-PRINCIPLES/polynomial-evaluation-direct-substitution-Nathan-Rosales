751ade0915c92ee9d3bd6c1d0ab199d1aa49baf6
Nathan-Rosales <nathan08.rosales@gmail.com>
2025-10-15 18:43:12 -0400

    Update polynomial_horner.py

--- polynomial_horner.py
+++ polynomial_horner.py
@@ -1,26 +1,30 @@
-def evaluate_polynomial_horner(degree, x, constant_term, *coefficients):
-    # TODO: Implement polynomial evaluation using Horner's method
+def evaluate_polynomial(degree, x, constant_term, *coefficients):
+    # TODO: Implement polynomial evaluation using direct substitution method
     # TODO: Print step-by-step evaluation (S0, S1, S2, etc.)
     # TODO: Return final polynomial result
-    
-    S = coefficients[degree-1]
+
+    if(len(coefficients)!=degree):
+        raise ValueError("needs " + str(degree) + " coefficient(s)")
+
+    S = constant_term
     k = 1
 
-    a = degree-2
+    a = 0
     while k<=degree:
         num = coefficients[a]
         S += num*(x**k)
-        a -= 1
+        a += 1
         k += 1
     
     return(S)
+    
 
 if __name__ == "__main__":
     # TODO: Add main program loop
     # TODO: Get user input for degree, x, constant term, and coefficients
-    # TODO: Call evaluate_polynomial_horner function
+    # TODO: Call evaluate_polynomial function
     # TODO: Ask user if they want to run again
-    
+
     answer = "y"
 
     while(answer=="y"):
@@ -33,6 +37,8 @@ if __name__ == "__main__":
         for i in range(1, degree+1):
             coefficients += (int(input(f"Coefficient of the x^{i} term: ")),)
         
-        print("P(x) = " + str(evaluate_polynomial_horner(degree, x, constant_term, coefficients)))
-
+        print("")
+        print("P(x) = " + str(evaluate_polynomial(degree, x, constant_term, coefficients)))
+        print("")
         answer = input("Do you want to evaluate another polynomial? (y/n): ")
+        print("")
